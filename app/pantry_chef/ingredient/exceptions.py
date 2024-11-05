7b34296ffb79d884ce9edc6c13b50da701953d70
Allan Aranzaso <aranzasoallan@gmail.com>
2024-11-04 21:43:40 -0800

    Remove custom exception
    Signed-off-by: Allan Aranzaso <aranzasoallan@gmail.com>

--- app/pantry_chef/ingredient/exceptions.py
+++ app/pantry_chef/ingredient/exceptions.py
@@ -13,14 +13,5 @@ class DBIngredientCreationFailed(Exception):
             self.detail = json.loads(db_err.json())
 
 
-class DBIngredientUpdateFailed(Exception):
-    detail: tuple[Any, ...]
-
-    def __init__(self, db_err: Exception) -> None:
-        self.detail = db_err.args
-        if not self.detail and isinstance(db_err, ValidationError):
-            self.detail = json.loads(db_err.json())
-
-
 class IngredientNotFoundException(Exception):
     pass
